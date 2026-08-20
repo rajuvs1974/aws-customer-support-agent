7ba0dae024ede8adfce459044888a2d1d83e4eee
Srinivasa Raju <rajuvs1974@gmail.com>
2026-08-20 16:36:01 -0500

    feat: add Bedrock Knowledge Base with S3 Vectors

---
+++ terraform/s3_vectors.tf
@@ -0,0 +1,21 @@
+resource "aws_s3vectors_vector_bucket" "knowledge" {
+  vector_bucket_name = "${var.project_name}-${var.environment}-vectors"
+}
+resource "aws_s3vectors_index" "knowledge" {
+  vector_bucket_name = aws_s3vectors_vector_bucket.knowledge.vector_bucket_name
+
+  index_name = "${var.project_name}-${var.environment}-index"
+
+  data_type = "float32"
+
+  dimension = 1024
+
+  distance_metric = "cosine"
+
+  metadata_configuration {
+    non_filterable_metadata_keys = [
+      "AMAZON_BEDROCK_TEXT",
+      "AMAZON_BEDROCK_METADATA"
+    ]
+  }
+}
