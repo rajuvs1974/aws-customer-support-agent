09f565b15369ee87bfb18721225e40bb198b8d4c
Srinivasa Raju <rajuvs1974@gmail.com>
2026-08-20 16:04:22 -0500

    feat: expose AI agent through API Gateway

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -14,3 +14,7 @@ output "orders_table" {
 output "shipments_table" {
   value = aws_dynamodb_table.shipments.name
 }
+output "api_url" {
+  description = "Customer support API URL"
+  value       = aws_apigatewayv2_stage.default.invoke_url
+}
